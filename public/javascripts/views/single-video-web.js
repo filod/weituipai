61273df18e9ec86c79b45111c83f443bd16ca6b0
filod lin <filod@qq.com>
2012-08-25 21:32:53 +0800

    cc

--- public/javascripts/views/single-video-web.js
+++ public/javascripts/views/single-video-web.js
@@ -1,4 +1,4 @@
-define(['jquery', 'backbone', 'hogan', 'models/Video'], 
+define(['jquery', 'backbone', 'hogan', 'models/video'], 
   function($, Backbone, hogan, Video) {
   var SingleVideoView = Backbone.View.extend({
     el: '#main-page',
